6351135d6ed5e009755d7f34dfeab8b1d71947e3
rqalqallaf <rq.alqallaf@gmail.com>
2022-03-03 18:59:17 +0300

    done

---
+++ first app/first app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first app
+//
+//  Created by GK on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Abdulrazaq alqallaf ")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first app/first app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first app
+//
+//  Created by GK on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
